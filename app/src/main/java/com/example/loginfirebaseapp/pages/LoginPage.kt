9d68f1996b780b20539aa36eca5573c81976d95d
ismailjd313 <ismailyousaf.jd313@gmail.com>
2025-01-27 18:05:23 +0500

    after implementing signup and login

--- app/src/main/java/com/example/loginfirebaseapp/pages/LoginPage.kt
+++ app/src/main/java/com/example/loginfirebaseapp/pages/LoginPage.kt
@@ -6,7 +6,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -20,6 +25,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,7 +68,11 @@ fun LoginPage(modifier: Modifier = Modifier, navController: NavController, authV
             label = {
                 Text(text = "Enter Your Email")
             },
-            singleLine = true
+            singleLine = true,
+            leadingIcon = {
+                Icon(Icons.Filled.Email, contentDescription = "email")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -72,7 +83,12 @@ fun LoginPage(modifier: Modifier = Modifier, navController: NavController, authV
             label = {
                 Text(text = "Enter Your Password")
             },
-            singleLine = true
+            singleLine = true,
+            leadingIcon = {
+                Icon(Icons.Filled.Lock, contentDescription = "password")
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
 
         Button(
--- app/src/main/java/com/example/loginfirebaseapp/pages/SignupPage.kt
+++ app/src/main/java/com/example/loginfirebaseapp/pages/SignupPage.kt
@@ -6,13 +6,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,11 +26,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.loginfirebaseapp.AuthState
 import com.example.loginfirebaseapp.AuthViewModel
+import java.security.cert.CertPathValidatorSpi
 
 @Composable
 fun SignupPage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
@@ -57,7 +66,11 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController, auth
             label = {
                 Text(text = "Enter Your Email")
             },
-            singleLine = true
+            singleLine = true,
+            leadingIcon = {
+                Icon(Icons.Filled.Email, contentDescription = "email")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -68,7 +81,12 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController, auth
             label = {
                 Text(text = "Enter Your Password")
             },
-            singleLine = true
+            singleLine = true,
+            leadingIcon = {
+                Icon(Icons.Filled.Lock, contentDescription = "password")
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
 
         Button(
