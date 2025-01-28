cd92594e6c37e053a428d6bbbd788ad4f9e00aa6
ismailjd313 <ismailyousaf.jd313@gmail.com>
2025-01-28 19:49:47 +0500

    after implementing signup and login

--- app/src/main/java/com/example/loginfirebaseapp/pages/SignupPage.kt
+++ app/src/main/java/com/example/loginfirebaseapp/pages/SignupPage.kt
@@ -18,7 +18,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
